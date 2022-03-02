05a6cb4adef7ad1a61f308101d5ccdd7ffd5d651
Noah Reece <noah.w.reece@gmail.com>
2022-03-02 18:51:59 -0500

    commit tree c79ad7bce99a427904401c3879a201c16ba631c0

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your code here
+function submitData(name, email) {
+    return fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+        },
+        body: JSON.stringify({
+            'name': name,
+            'email': email
+        })
+    })
+    .then( res => res.json())
+    .then( data => {
+        const p = document.createElement('p');
+        p.textContent = data.id;
+        document.getElementsByTagName('body')[0].appendChild(p);
+    })
+    .catch( error => {
+        const p = document.createElement('p');
+        p.textContent = error.message;
+        document.getElementsByTagName('body')[0].appendChild(p);
+        console.log(error.message);
+    });
+}
